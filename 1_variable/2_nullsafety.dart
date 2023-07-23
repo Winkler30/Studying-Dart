212c07fa172b820c2fec18b784b213e63433e727
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-23 11:06:49 -0300

    Nullsafety

---
+++ 1_variable/2_nullsafety.dart
@@ -0,0 +1,16 @@
+// Variables of higher level don't need to be initialized after 
+//Don't is permitted
+// String completeNameHigher;
+void main() {
+
+  // Local variables don't need to be initialized at face value but
+  // can be created without a value and assigned a value later.
+  String completeName;
+  var a;
+  var b;
+
+  completeName = 'Charles David Winkler';
+  a = 1;
+  b = 1.2;
+  print(completeName.length);
+}
