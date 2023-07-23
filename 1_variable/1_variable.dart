6f44b86ed2695aac00bff9994df599631e9456ed
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-23 10:37:11 -0300

    Initial study Dart

---
+++ 1_variable/1_variable.dart
@@ -0,0 +1,25 @@
+// Integer 1,2,3,4,5,6,7,8,9,10
+// Decimal 1.1,1.2,1.3,1.4,1.5
+// Function Main is used for start
+// Type + Name Variable = Value
+// snake_case default
+// camelCase default
+// Everyone in Dart is class
+// int and double extends num
+void main() {
+  num anyNumber = 10;
+  int age = 19;
+  double valueIphone = 1.100;
+  //camelCase
+  String nameCourse = 'Studying Flutter';
+  //snake_case
+  String name_course = 'Studying Flutter';
+
+  // Right Way for declaring a variable
+  var name = 'Charles David Winkler';
+  var height = '1.70';
+
+  // Everyone is child of Object
+  Object objectAny = 1.2; //Take any type value
+  dynamic dynamicAny = 'Charles'; //Take any type value
+}
